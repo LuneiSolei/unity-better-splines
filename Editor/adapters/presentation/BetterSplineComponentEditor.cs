1863735a7cb1450687303c2a9a87569b94b9e45b
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2025-10-30 14:31:23 -0500

    chore: cleanup unused consts

--- Editor/adapters/presentation/BetterSplineComponentEditor.cs
+++ Editor/adapters/presentation/BetterSplineComponentEditor.cs
@@ -19,13 +19,11 @@ namespace LuneiSolei.BetterSplines.Editor.Adapters
         private static class FieldProperties
         {
             public const string SplineIndex = "splineIndex";
-            public const string DefaultNodeSpacing = "defaultNodeSpacing";
         }
 
         private static class FieldNames
         {
             public const string SplineDropdown = "SplineDropdown";
-            public const string SpacingEnum = "SpacingEnum";
             public const string NodeList = "NodeList";
         }
         
