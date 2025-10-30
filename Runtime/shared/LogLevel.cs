2a3da608f6b00eeb09c5706cd228a5b704687ce6
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2025-10-30 11:23:55 -0500

    feat: add a custom structured logger for debugging and general logging

---
+++ Runtime/shared/LogLevel.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace LuneiSolei.BetterSplines.Shared
+{
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warning,
+        Error
+    }
+}
---
+++ Runtime/shared/Logger.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+
+namespace LuneiSolei.BetterSplines.Shared
+{
+    public static class Logger
+    {
+        private static void Log(
+            string message,
+            LogLevel level = LogLevel.Debug,
+            Dictionary<string, object> data = null,
+            UnityEngine.Object context = null)
+        {
+            // Create the full message
+            string msgPrefix = $"[{InternalConfig.PackageName}] [{level}]";
+            string msg = $"{msgPrefix} {message}";
+
+            // Attach any relevant data, if any
+            if (data is { Count: > 0 })
+            {
+                msg += "\n    Data: " + FormatData(data);
+            }
+
+            switch (level)
+            {
+                case LogLevel.Debug:
+                case LogLevel.Info:
+                    UnityEngine.Debug.LogFormat(msg, context);
+                    break;
+                case LogLevel.Warning:
+                    UnityEngine.Debug.LogWarningFormat(msg, context);
+                    break;
+            }
+        }
+        
+        private static string FormatData(Dictionary<string, object> data)
+        {
+            List<string> dataPairs = new();
+            foreach ((string key, object value) in data)
+            {
+                dataPairs.Add($"{key}={value ?? "null"}");
+            }
+            
+            return string.Join(", ", dataPairs);
+        }
+
+        // Convenience Logging Methods
+        internal static void Debug(
+            string message,
+            Dictionary<string, object> data = null,
+            UnityEngine.Object context = null)
+        {
+            Log(message, LogLevel.Debug, data, context);
+        }
+
+        internal static void Warning(
+            string message,
+            Dictionary<string, object> data = null,
+            UnityEngine.Object context = null)
+        {
+            Log(message, LogLevel.Warning, data, context);
+        }
+
+        internal static void Error(
+            string message,
+            Dictionary<string, object> data = null,
+            UnityEngine.Object context = null)
+        {
+            Log(message, LogLevel.Error, data, context);
+        }
+        
+        // Specific Message Methods
+        internal static void NodePrefabIsNull(UnityEngine.Object context = null)
+        {
+            Warning(message: "Spline node prefab is null", context: context);
+        }
+    }
+}
