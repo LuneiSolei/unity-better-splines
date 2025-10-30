23b8cfc91d91df605de86d9e8a749ef3217a0713
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2025-10-30 11:17:48 -0500

    feat: allow the component to update and spawn spline nodes whenever there is a change in the relevant spline

--- Runtime/adapters/presentation/BetterSplineComponent.cs
+++ Runtime/adapters/presentation/BetterSplineComponent.cs
@@ -33,20 +33,71 @@ namespace LuneiSolei.BetterSplines.Adapters
             }
         }
         
-        [SerializeField]
-        private int splineIndex;
-        public int SplineIndex
+        /// <summary>
+        /// Updates all spline nodes along the assigned spline.
+        /// </summary>
+        private void UpdateNodes()
         {
-            get => splineIndex;
-            set => splineIndex = value;
+            Shared.Logger.Debug(
+                message: "Updating nodes",
+                data: new Dictionary<string, object>
+                {
+                    {"splineIndex", splineIndex},
+                    {"splineContainer", splineContainer}
+                },
+                context: this);
+            
+            // Spawn each node in the spline nodes list
+            for (int i = 0; i < splineNodes.Count; i++)
+            {
+                // Spawn the spline node
+                SplineNode node = splineNodes[i];
+                float ratio = EvaluateRatio(i);
+                node.ValidateGameObject(defaultGameObject);
+                node.UpdatePosition(AssignedSpline, ratio, splineContainer.transform);
+                node.Spawn();
+            }
+        }
+
+        /// <summary>
+        /// Evaluates a ratio along the assigned spline. The ratio is dependent upon the selected node spacing method.
+        /// </summary>
+        /// <param name="index">The index of the spline node to evaluate.</param>
+        /// <returns>A ratio representative of the relevant spline node's position along the assigned spline.</returns>
+        private float EvaluateRatio(int index)
+        {
+            switch (defaultNodeSpacing)
+            {
+                case NodeSpacing.EvenSpacing:
+                    return (float)index / (splineNodes.Count - 1);
+            }
+
+            return 0.0f;
         }
         
-        [SerializeField]
-        private GameObject defaultGameObject;
-        public GameObject DefaultGameObject
+        /// <summary>
+        /// Called when the assigned spline has been modified.
+        /// </summary>
+        /// <param name="changedSpline">The spline that was modified.</param>
+        /// <param name="knotIndex">The index of the knot that was changed.</param>
+        /// <param name="modification">An object containing information about the modification(s).</param>
+        private void OnSplineChanged(Spline changedSpline, int knotIndex, SplineModification modification)
         {
-            get => defaultGameObject;
-            set => defaultGameObject = value;
+            // Ensure the changed spline matches the one from our cached index
+            if (!splineContainer.Splines[splineIndex].Equals(changedSpline)) return;
+
+            UpdateNodes();
+        }
+        
+        private void OnEnable()
+        {
+            UpdateNodes();
+            Spline.Changed += OnSplineChanged;
+        }
+
+        private void OnDisable()
+        {
+            Spline.Changed -= OnSplineChanged;
         }
         
         private void Reset()
